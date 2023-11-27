a8a151360b12ea7aa01646416574a31023d37904
gidmak <gideonmarkus@gmail.com>
2023-11-28 00:56:19 +0100

    day 1 exercise

---
+++ day_1/helloworld.py
@@ -0,0 +1,56 @@
+#Day 1 of 30 days of python
+
+# Addition
+print(3 + 4) 
+
+#Subtraction 
+print(4 - 3)
+
+# Multiplication
+print(4 * 3)
+
+# Modulus
+print(4 % 3)
+
+# Exponential
+print( 4 ** 3)
+
+# Floor division
+print(4 // 3)
+
+# Writing strings
+# My name 
+print("Gideon")
+
+# Family name
+print("Markus")
+
+# Country
+print("Nigeria")
+
+# Experience of 30 days of python
+print("I am enjoying 30 days of python")
+
+# Checking data types
+
+# Int
+print(type(10))
+
+# Float
+print(type(9.8))
+
+# Float
+print(type(3.14))
+
+# Complex 
+print(type(4 - 4j))
+
+# List
+print(type(['Asebeneh', 'Python', 'Finland']))
+
+# Strings
+print(type("Gideon"))
+
+print(type("Markus"))
+
+print(type("Nigeria"))
