0d0bf799cf822ea2f8e6af787b224881c6150081
gidmak <gideonmarkus@gmail.com>
2023-12-02 23:51:50 +0100

    Added string functionalities

---
+++ day_4/strings.py
@@ -0,0 +1,11 @@
+# Day 4 of 30 Days of python
+
+# # Convert '30', 'days', 'of', 'python' to a single string
+# broken_strings1 = ['30', 'Days', 'Of', 'Python']
+# conc_string1 = ' '.join(broken_strings1)
+# print(conc_string1)
+
+# Concatenate 'Coding' 'For' 'All' into a single string
+broken_strings2 = ['Coding', 'For', 'All']
+conc_string2 = ' '.join(broken_strings2)
+print(conc_string2)
