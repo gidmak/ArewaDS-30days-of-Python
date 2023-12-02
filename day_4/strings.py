8f49a274df6c0e6b42e0f5bb15799fab17674d5e
gidmak <gideonmarkus@gmail.com>
2023-12-02 23:55:42 +0100

    Added string declarations

--- day_4/strings.py
+++ day_4/strings.py
@@ -5,7 +5,16 @@
 # conc_string1 = ' '.join(broken_strings1)
 # print(conc_string1)
 
-# Concatenate 'Coding' 'For' 'All' into a single string
-broken_strings2 = ['Coding', 'For', 'All']
-conc_string2 = ' '.join(broken_strings2)
-print(conc_string2)
+# # Concatenate 'Coding' 'For' 'All' into a single string
+# broken_strings2 = ['Coding', 'For', 'All']
+# conc_string2 = ' '.join(broken_strings2)
+# print(conc_string2)
+
+# Declare a variable named company and assign it to an initial value "Coding For All"
+company = 'Coding For All'
+
+# Print the variable company using print()
+print(company)
+
+# Print the length of the company string using len() method and print()
+print(len(company))
