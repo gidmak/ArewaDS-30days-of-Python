fdf00143c634c4bc35b109e10121086f10472d04
gidmak <gideonmarkus@gmail.com>
2023-12-04 03:18:10 +0100

    Added second level exercise

--- day_5/lists.py
+++ day_5/lists.py
@@ -1,19 +1,19 @@
 # Day 5 of 30 Days of python
 
-# # Declare an empty list
-# empty_list = []
+# Declare an empty list
+empty_list = []
 
 # Declare a list with more than 5 items
 list_more_than_five = ["arsenal", "liverpool", "manchester city", "aston villa", "tottenham"]
 
-# # Find the length of your list
-# print(len(list_more_than_five))
+# Find the length of your list
+print(len(list_more_than_five))
 
-# # Get the first item, the middle item and the last item of the list
-# print(list_more_than_five[::2])
+# Get the first item, the middle item and the last item of the list
+print(list_more_than_five[::2])
 
-# # Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
-# mixed_data_types = ['Gideon', '34', '6.9', 'married', 'kaduna']
+# Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Gideon', '34', '6.9', 'married', 'kaduna']
 
 # Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
 it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
@@ -95,3 +95,241 @@ print(front_end)
 full_stack = front_end[:]
 full_stack.insert(4, ['Python', 'SQL'])
 print(full_stack)
+
+# The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+# Sort the list and find the min and max age
+ages.sort()
+print(ages)     # min = 19; max = 26
+
+# Add the min age and the max age again to the list
+min_max = [19, 26]
+ages.extend(min_max)
+print(ages)
+
+# Find the median age (one middle item or two middle items divided by two)
+median_age = ages[4:6]
+print(sum(median_age) / 2)
+
+# Find the average age (sum of all items divided by their number )
+average = sum(ages) / len(ages)
+print(average)
+
+# Find the range of the ages (max minus min)
+range_of_numbers = max(min_max) - min(min_max)
+print(range_of_numbers)
+
+# Compare the value of (min - average) and (max - average), use abs() method
+min_diff = abs(min(min_max) - average)
+max_diff = abs(max(min_max) - average)
+print(min_diff)
+print(max_diff)
+
+# Find the middle country(ies) in the countries list
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
+]
+middle_country = countries[96:97]
+print(middle_country)
+
+# Divide the countries list into two equal lists if it is even if not one more country for the first half.
+first_list = countries[:97]
+second_list = countries[97:]
+print(f"first list is {len(first_list)} while second list is {len(second_list)}")
+
+# ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+first, second, third, *scandic = countries
+print(f'first is {first}, second is {second}, third is {third} and the scandic countries are {len(scandic)}')
