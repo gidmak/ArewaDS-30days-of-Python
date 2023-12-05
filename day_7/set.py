c5ce8ed9c8ff6b4b543b1c2e6859cbb5bbb2a828
gidmak <gideonmarkus@gmail.com>
2023-12-05 03:34:49 +0100

    Added second level exercise

--- day_7/set.py
+++ day_7/set.py
@@ -2,9 +2,6 @@
 
 # sets
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-A = {19, 22, 24, 20, 25, 26}
-B = {19, 22, 20, 25, 26, 24, 28, 27}
-age = [22, 19, 24, 25, 26, 24, 25, 24]
 
 # Find the length of the set it_companies
 print(len(it_companies))    # 7
@@ -27,3 +24,29 @@ print(it_companies)
 
 it_companies.discard({"Pinta"})
 print(it_companies)             # No error raised
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+# Join A and B
+print(A.union(B))           # {19, 20, 22, 24, 25, 26, 27, 28}
+
+# Find A intersection B
+print(A.intersection(B))    # {19, 20, 22, 24, 25, 26}
+
+# Is A subset of B
+print(A.issubset(B))        # True
+
+# Are A and B disjoint sets
+print(A.isdisjoint(B))      # False
+
+# Join A with B and B with A
+print(A.union(B))           # {19, 20, 22, 24, 25, 26, 27, 28}
+print(B.union(A))           # print(B.union(A))
+
+# What is the symmetric difference between A and B
+print(A.symmetric_difference(B))    # {27, 28}
+
+# Delete the sets completely
+del A,B
